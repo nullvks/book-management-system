269c1ecaa085df08477f2f885bcd5c3e49469485
nullvks <andrewhernando@protonmail.com>
2024-12-29 21:43:45 +0800

    update test

--- src/main/java/com/nullvks/bookmanagementsystem/BookmanagementsystemApplication.java
+++ src/main/java/com/nullvks/bookmanagementsystem/BookmanagementsystemApplication.java
@@ -8,6 +8,7 @@ public class BookmanagementsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagementsystemApplication.class, args);
+		System.out.println("test");
 	}
 
 }
