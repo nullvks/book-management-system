9fe0882bdb099d3ad4980edb1bae26f344d7a037
nullvks <andrewhernando@protonmail.com>
2025-02-04 21:41:45 +0700

    Added entity, db config

---
+++ src/main/java/com/nullvks/bookmanagementsystem/entity/Book.java
@@ -0,0 +1,25 @@
+package com.nullvks.bookmanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "book_library")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title", nullable = false, unique = true)
+    private String title;
+
+    @Column(name = "author_name")
+    private String authorName;
+}
